67eb7e319295d2fffc03fd3f11d8270df9297c07
Marco Frassoni <marco.frassoni@gmail.com>
2010-11-11 13:51:50 +0100

    adding a few more columns, introducing threshold option

--- usercontributions_export.py
+++ usercontributions_export.py
@@ -26,7 +26,7 @@ from wbin import deserialize
 from django.utils.encoding import smart_str
 
 import sonet.mediawiki as mwlib
-from sonet.lib import find_open_for_this_file
+from sonet.lib import find_open_for_this_file, yyyymmdd_to_datetime
 
 def user_iter(lang = 'en', paginate=10000000):
     contrib, conn = get_contributions_table()
@@ -54,17 +54,44 @@ def user_iter(lang = 'en', paginate=10000000):
             ))) if v['namespace_edits'] is not None else None
             yield v
 
-def prepare_data(namespaces, lang):
+def prepare_data(namespaces, lang, date_, threshold = 0):
     for user in user_iter(lang=lang):
+                
+        if user['normal_edits']:
+            user['tot_edits'] = user['normal_edits']
+            user['diversity_score'] = 1
+        else:
+            user['tot_edits'] = 0
+            user['diversity_score'] = 0
+            
         if user['namespace_edits'] is None:
             user['namespace_edits'] = [0,]*len(namespaces)
+            
         for i, namespace_edit in enumerate(user['namespace_edits']):
             user[namespaces[i]] = namespace_edit
+            user['tot_edits'] += namespace_edit
+            if namespace_edit: 
+                user['diversity_score'] += 1
+            
         del user['namespace_edits']
+        
+        if user['tot_edits'] < threshold:
+            continue
 
         ## smart_str to manage unicode
         user['username'] = smart_str(user['username'])
 
+        user['days_since_first_edit'] = (date_ - user['first_edit']).days
+        user['left_since'] = (date_ - user['last_edit']).days
+        user['active_days'] = (user['last_edit']- user['first_edit']).days
+        
+        user['first_edit_year'] = user['first_edit'].year
+        user['first_edit_month'] = user['first_edit'].month
+        user['first_edit_day'] = user['first_edit'].day
+        user['last_edit_year'] = user['last_edit'].year
+        user['last_edit_month'] = user['last_edit'].month
+        user['last_edit_day'] = user['last_edit'].day
+        
         ## converts datetime objects to timestamps (seconds elapsed since
         ## 1970-01-01)
         user['first_edit'] = int(time.mktime(user['first_edit'].timetuple()))
@@ -72,15 +99,19 @@ def prepare_data(namespaces, lang):
 
         yield user
 
-def get_xml_file():
-    from optparse import OptionParser
-    op = OptionParser("%prog dump.xml.gz output_file.bz2",
-        description="Export User contribution data from database into csv")
-    _, args = op.parse_args()
-    if len(args) != 2:
-        op.error('Missing xml dump file or output file')
-    return args[0], args[1]
+def create_option_parser():
+    import argparse
+        
+    p = argparse.ArgumentParser(description='Export User contribution data from database into csv')
 
+    ## optional parameters
+    p.add_argument('-t', '--threshold', help='total edits threshold (default: %(default)s)', type=int, default=0)
+    ## positional arguments
+    p.add_argument('dump', help="dump file", metavar="DUMP_FILE")
+    p.add_argument('out', help="output file (bz2 ext)", metavar="OUTPUT_FILE")
+    
+    return p        
+    
 def main():
     from bz2 import BZ2File
     from csv import DictWriter
@@ -90,10 +121,15 @@ def main():
                         level=logging.DEBUG)
     logging.info('---------------------START---------------------')
 
-    xml, out = get_xml_file()
-
+    op = create_option_parser()
+    args = op.parse_args()
+       
+    xml, out, threshold = args.dump, args.out, args.threshold
+    
     lang, date_, type_ = mwlib.explode_dump_filename(xml)
     deflate, _lineno = find_open_for_this_file(xml)
+    
+    date_ = yyyymmdd_to_datetime(date_)
 
     if _lineno:
         src = deflate(xml, 51)
@@ -106,7 +142,10 @@ def main():
 
     fields = ['username', 'normal_edits', 'comments_count', 'comments_avg',
               'minor', 'revert', 'npov', 'welcome', 'please', 'thanks',
-              'first_edit', 'last_edit']
+              'first_edit', 'last_edit', 'tot_edits', 'active_days', 
+              'days_since_first_edit', 'left_since', 'diversity_score',
+               'first_edit_year', 'first_edit_month', 'first_edit_day', 
+               'last_edit_year', 'last_edit_month', 'last_edit_day',]
     fields[2:2] = namespaces
     dw = DictWriter(fout, fields)
     dw.writeheader()
@@ -114,7 +153,7 @@ def main():
     ## to get only the first 1000 users:
     #from itertools import islice
     #data_iterator = islice(prepare_data(namespaces), 1000)
-    data_iterator = prepare_data(namespaces, lang)
+    data_iterator = prepare_data(namespaces, lang, date_, threshold)
     
     count = 0
     for user in data_iterator:
