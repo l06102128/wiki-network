927aa795c464735854a75feee2d1082e419f0865
fox <fox91@anche.no>
2010-12-27 17:53:21 +0100

    added GPL license

--- revisions_page.py
+++ revisions_page.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+
+##########################################################################
+#                                                                        #
+# This program is free software; you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation; version 2 of the License.                #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           #
+# GNU General Public License for more details.                           #
+#                                                                        #
+##########################################################################
+
 from sonet.mediawiki import HistoryPageProcessor, \
                             get_translations, get_tags, \
                             explode_dump_filename, only_inserted_text
