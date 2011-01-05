fd5e30c1c4d16f611d8d1125ccf050a72492fdb9
fox <fox91@anche.no>
2011-01-05 14:18:30 +0100

    added pywc :D

--- pywc.py
+++ pywc.py
@@ -2,7 +2,7 @@
 
 ##########################################################################
 #                                                                        #
-# Copyleft Federico "fox" Scrinzi - 2011                                 #
+# Copyleft Federico "fox" Scrinzi (fox91 at anche dot no)                #
 #                                                                        #
 # pyWC is free software; you can redistribute it and/or modify           #
 # it under the terms of the GNU General Public License as published by   #
