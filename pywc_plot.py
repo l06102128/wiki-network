d581c3f8d7b3347b3eb84b186048a3c3a58043d6
fox <fox91@anche.no>
2011-01-27 19:08:02 +0100

    fixed a bit, still quite ugly

--- pywc_plot.py
+++ pywc_plot.py
@@ -7,22 +7,16 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as md
 from datetime import datetime as dt
 from datetime import timedelta
-
 import csv
 import numpy as np
 
-def calc_perc(n, total, perc):
-    if perc:
-        try:
-            return float(n)/float(total)*100
-        except ZeroDivisionError:
-            return 0
-    else:
-        return float(n)
 
-def collapse_values(timestamps, values, radius):
+# Quite ugly, TODO: if it's possible use groupby
+# [list(e) for k,e in groupby([11,12,13,21],(lambda x : x//10 ))]
+def collapse_values(timestamps, values, totals, radius):
     t = []
     s = []
+    tot = []
     delta = timedelta(days=radius)
     first = timestamps[0]
     curr = []
@@ -31,12 +25,22 @@ def collapse_values(timestamps, values, radius):
         if (j - first) < delta:
             curr.append(j)
         else:
-            t.append(curr[-1])
+            t.append(curr[-1])  # Use last timestamp of the group
+            # Sum values and totals of the current group
             s.append(sum([x for x in values[i:i+len(curr)]]))
+            tot.append(sum([x for x in totals[i:i+len(curr)]]))
         first = j
         i += 1
-    print t, s
-    return t, s
+    return t, s, tot
+
+def _gen_data(line, id_col, ignorecols, onlycols):
+    for i, e in enumerate(line):
+        if i != id_col and \
+           (not ignorecols or not i in ignorecols) and \
+           (not onlycols or i in onlycols) and \
+           i != len(line) - 1 and  \
+           i != len(line) - 2: # don't count last two cols
+            yield e
 
 def main():
     import optparse
@@ -76,31 +80,22 @@ def main():
     # content contains all the csv file
     content = [row for i, row in enumerate(csv_reader)]
 
-    header = [name for j, name in enumerate(content[0])
-              if j != opts.id_col and \
-                 (not ignorecols or not j in ignorecols) and \
-                 (not onlycols or j in onlycols) and
-                 j != len(content[0]) - 1 and  # don't count last two columns
-                 j != len(content[0]) - 2]     # (total and text)
+    header = [x for x in _gen_data(content[0], opts.id_col,
+                                   ignorecols, onlycols)]
 
     # Creates a matrix (list) with percentages of the occurrencies of every
     # category. Don't count id, total, text, ignore columns. If onlycols is set
     # consider only them.
     mat = []
     timestamps = []
+    totals = []
 
     for line in content[1:]:
-        mat.append([calc_perc(e, line[-2], opts.perc) for i, e \
-                    in enumerate(line) \
-                    if i != opts.id_col and \
-                       (not ignorecols or not i in ignorecols) and \
-                       (not onlycols or i in onlycols) and
-                       i != len(line) - 1 and  # don't count last two cols
-                       i != len(line) - 2])    # (total and text)
+        mat.append([x for x in _gen_data(line, opts.id_col,
+                                         ignorecols, onlycols)])
+        totals.append(float(line[-2]))
         timestamps.append(dt.strptime(line[opts.id_col],
                           "%Y-%m-%dT%H:%M:%SZ"))
-
-    collapse_values(timestamps, [], 5)
 
     mat = np.array(mat, dtype=np.float).transpose()
 
@@ -109,20 +104,27 @@ def main():
         plt.clf()
         plt.subplots_adjust(bottom=0.2)
         plt.xticks(rotation=25)
-        ax=plt.gca()
+        ax = plt.gca()
         xfmt = md.DateFormatter('%Y-%m-%d')
         ax.xaxis.set_major_formatter(xfmt)
-        ax.yaxis.set_data_interval(0.0001, None, False)
-        if opts.perc:
-            plt.ylabel("%")
-        else:
-            plt.xlabel("")
+        #ax.yaxis.set_data_interval(0.0001, None, False)
         plt.xlabel("Revisions Timestamp")
 
-        s = [x for x in series if x != 0]  # Don't plot zeros!
+        # Don't plot zeros and skip zero revisions!
+        s = [x for x in series if x != 0]
         t = [x for k, x in enumerate(timestamps) if series[k] != 0]
+        tot = [x for k, x in enumerate(totals) if series[k] != 0]
+
         if opts.window:
-           t, s = collapse_values(t, s, opts.window)
+            t, s, tot = collapse_values(t, s, tot, opts.window)
+
+        if opts.perc:
+            # Calculate percentages
+            s = [x/tot[k]*100 for k, x in enumerate(s)]
+            # Set axis limit 0-100 FIXME IS IT GOOD OR BAD?
+            ax.set_ylim(0, 100)
+            plt.ylabel("%")
+
         if t and s:
             plt.plot(matplotlib.dates.date2num(t), s, "-o")
             plt.title(header[i])
