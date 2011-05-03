3717004add8dc33febfc359bcde2a50d2ee89c9e
fox <fox91@anche.no>
2011-05-03 15:56:32 +0200

    csv manipulation split functionalities

--- csv_manipulation.py
+++ csv_manipulation.py
@@ -14,61 +14,28 @@ RWORDS = re.compile(r"\S+")
 def words(text):
     return [word for word in RWORDS.findall(text)]
 
-def main():
-    import optparse
-    from sonet.lib import SonetOption
+def get_stats(opts, files):
+    csv_reader = csv.reader(open(files[0], 'r'),
+                            delimiter=opts.delimiter,
+                            quotechar=opts.quotechar,
+                            quoting=csv.QUOTE_ALL)
+    pages = set()
+    counter = 0
+    for line in csv_reader:
+        pages.add((line[2], line[3]))
+        counter += 1
+    return {"pages": pages, "lines": counter}
 
-    # Parameters
-    p = optparse.OptionParser(usage="usage: %prog [options] file",
-                              option_class=SonetOption)
-    p.add_option('-d', '--delimiter', action="store", dest="delimiter",
-                 default="\t", help="CSV delimiter")
-    p.add_option('-q', '--quotechar', action="store", dest="quotechar",
-                 default='"', help="CSV quotechar")
-    p.add_option('-l', '--lines', action="store", dest="lines",
-                 type="int", help="Number of lines to print")
-    p.add_option('-p', '--page', action="store", dest="page",
-                 help="Select a specific page")
-    p.add_option('-t', '--type', action="store", dest="type",
-                 help="Select a specific page type (normal|talk)")
-    p.add_option('-i', '--info', action="store_true", dest="info",
-                 help="Get info about CSV file")
-    p.add_option('-S', '--start-line', action="store", dest="start_line",
-                 type="int", help="Skip lines before START_LINE")
-    p.add_option('-w', '--words-window', action="store", dest="words_window",
-                 type="int", help="Set a word window")
-    p.add_option('-H', '--header', action="store_true", dest="header",
-                 help="Output header")
-    p.add_option('-s', '--start', action="store",
-        dest='start', type="yyyymmdd", metavar="YYYYMMDD", default=None,
-        help="Look for revisions starting from this date")
-    p.add_option('-e', '--end', action="store",
-        dest='end', type="yyyymmdd", metavar="YYYYMMDD", default=None,
-        help="Look for revisions until this date")
-    opts, files = p.parse_args()
-    if len(files) != 1:
-        p.error("Wrong parameters")
-
+def extract_page(opts, files, output=stdout):
     # CSV handlers
     csv_reader = csv.reader(open(files[0], 'r'),
                             delimiter=opts.delimiter,
                             quotechar=opts.quotechar,
                             quoting=csv.QUOTE_ALL)
-    csv_writer = csv.writer(stdout,
+    csv_writer = csv.writer(output,
                             delimiter=opts.delimiter,
                             quotechar=opts.quotechar,
                             quoting=csv.QUOTE_ALL)
-    csv.field_size_limit(1000000000)  # Used for big cells, prevents exception
-
-    # If needed prints info and exits
-    if opts.info:
-        counter = 0
-        for line in csv_reader:
-            counter += 1
-        print "CSV info:"
-        print "Number of lines: %d" % counter
-        exit(0)
-
     # Print only intresting lines!
     # Really ugly but doesn't eat memory
     i = 0
@@ -107,46 +74,66 @@ def main():
                         counter = 0
                 if len_queue == 0 and counter == 0:
                     queue = None
-
-                """
-                wline = words(line[-1])
-                if len(wline) < opts.words_window:
-                    if not queue:
-                        # If queue is empty set initial value
-                        print "SETTING INITIAL QUEUE = LINE"
-                        queue = line
-                    else:
-                        #if len(words(queue[-1])) + len(wline) < opts.words_window:
-                        print "ADDING TEXT TO QUEUE"
-                        # add revision to queue
-                        queue[-1] += " " + " ".join(wline)
-                else:
-                    # If revision is too big leave it as its
-                    print "OUTPUTTING QUEUE AND LINE!"
-                    if queue:
-                        csv_writer.writerow(queue)
-                        queue = None
-                        i += 1
-                    csv_writer.writerow(line)
-                    i += 1
-                """
             else:
                 csv_writer.writerow(line)
     if queue:
         csv_writer.writerow(queue)
 
-    # hungry of memory
-    """
-    if opts.page is not None:
-        out = [line for line in csv_reader if line[2] == opts.page]
+def main():
+    import optparse
+    from sonet.lib import SonetOption
+
+    # Parameters
+    p = optparse.OptionParser(usage="usage: %prog [options] file",
+                              option_class=SonetOption)
+    p.add_option('-d', '--delimiter', action="store", dest="delimiter",
+                 default="\t", help="CSV delimiter")
+    p.add_option('-q', '--quotechar', action="store", dest="quotechar",
+                 default='"', help="CSV quotechar")
+    p.add_option('-l', '--lines', action="store", dest="lines",
+                 type="int", help="Number of lines to print")
+    p.add_option('-p', '--page', action="store", dest="page",
+                 help="Select a specific page")
+    p.add_option('-t', '--type', action="store", dest="type",
+                 help="Select a specific page type (normal|talk)")
+    p.add_option('-i', '--info', action="store_true", dest="info",
+                 help="Get info about CSV file")
+    p.add_option('-S', '--start-line', action="store", dest="start_line",
+                 type="int", help="Skip lines before START_LINE")
+    p.add_option('-w', '--words-window', action="store", dest="words_window",
+                 type="int", help="Set a word window")
+    p.add_option('-H', '--header', action="store_true", dest="header",
+                 help="Output header")
+    p.add_option('--split', action="store_true", dest="split_file",
+                 help="Split csv into different files, one per page")
+    p.add_option('-s', '--start', action="store",
+        dest='start', type="yyyymmdd", metavar="YYYYMMDD", default=None,
+        help="Look for revisions starting from this date")
+    p.add_option('-e', '--end', action="store",
+        dest='end', type="yyyymmdd", metavar="YYYYMMDD", default=None,
+        help="Look for revisions until this date")
+    opts, files = p.parse_args()
+    if len(files) != 1:
+        p.error("Wrong parameters")
+
+    csv.field_size_limit(1000000000)  # Used for big cells, prevents exception
+
+    # If needed prints info and exits
+    if opts.info:
+        stats = get_stats(opts, files)
+        print "CSV info:"
+        print "Number of lines: %d" % stats["lines"]
+        print "\nPages: "
+        print "\n".join(["%s (%s)" % (p[0], p[1]) for p in stats["pages"]])
+    elif opts.split_file:
+        stats = get_stats(opts, files)
+        for page in stats["pages"]:
+            fn = "%s-%s_%s.csv" % (files[0].split(".csv")[0], page[0], page[1])
+            opts.page, opts.type = page
+            extract_page(opts, files, open(fn, "w"))
     else:
-        out = [line for line in csv_reader]
-    l = len(out)
-    if opts.lines > 0 and opts.lines <= len(out):
-        l = opts.lines
-    for e in out[:opts.lines]:
-        csv_writer.writerow(e)
-    """
+        extract_page(opts, files)
+
 
 if __name__ == '__main__':
     main()
