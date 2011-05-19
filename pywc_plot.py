8bc17663094208e662acc7c637e15ef13dcc05dd
phauly <paolo@gnuband.org>
2011-05-19 16:44:02 +0200

    added parameters --exclude-more-than --exclude-less-than. They are used to specify to exclude lines with totals (or dic if -d parameter is used) more or less than a specified threshold.

--- pywc_plot.py
+++ pywc_plot.py
@@ -90,7 +90,7 @@ def main():
     p.add_option('-v', action="store_true", dest="verbose", default=False,
                  help="Verbose output")
     p.add_option('-i', '--ignorecols', action="store", dest="ignorecols",
-                 help="Coulmns numbers of the source file to ignore"
+                 help="Columns numbers of the source file to ignore"
                       "(comma separated and starting from 0)")
     p.add_option('-I', '--id', action="store", dest="id_col", type="int",
                  help="Id column number (starting from 0)", default=0)
@@ -101,6 +101,12 @@ def main():
                  help="Use percentages instead of absolute value")
     p.add_option('-w', '--window', action="store", dest="window", type=int,
                  help="Collapse days")
+    p.add_option('--exclude-less-than', action="store", dest="excludelessthan", type=int,
+                 help="Exclude lines with totals (or dic if -d option is used) " + \
+		 "smaller than this parameter")
+    p.add_option('--exclude-more-than', action="store", dest="excludemorethan", type=int,
+                 help="Exclude lines with totals (or dic if -d option is used) " + \
+		 "greater than this parameter")
     p.add_option('-s', '--start', action="store",
         dest='start', type="yyyymmdd", metavar="YYYYMMDD", default=None,
         help="Look for revisions starting from this date")
@@ -144,6 +150,14 @@ def main():
         tot_index = -4
 
     for line in content[1:]:
+        #filter only pages with total (or dic is -d) greater or smaller than X
+        if opts.excludemorethan:
+            if float(line[tot_index]) > opts.excludemorethan:
+	        continue
+        if opts.excludelessthan:
+            if float(line[tot_index]) < opts.excludelessthan:
+	        continue
+
         mat.append([x for x in _gen_data(line, opts.id_col,
                                          ignorecols, onlycols)])
         totals.append(float(line[tot_index]))
@@ -183,7 +197,6 @@ def main():
             if opts.perc:
                 try:
                     mean = float(sum(series)) / sum(totals)
-                    #rel_mean = float(sum(ser)) / len(ser)
                     rel_mean = float(sum(ser)) / sum(tot)
                 except ZeroDivisionError:
                     mean = 0
