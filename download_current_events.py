50b12b432d597ed70bf1b80d7896a1539c070fbc
fox <fox91@anche.no>
2011-06-27 21:53:24 +0800

    script to download current events

---
+++ download_current_events.py
@@ -0,0 +1,52 @@
+import simplejson
+import logging
+import urllib
+import sys
+
+
+def get_data(output, lang="en", eititle="Template:Current", eicontinue=None):
+    """Function that extracts from the Wikipedia API which pages embed
+       a specific template
+    """
+    done = False
+    api_base = 'http://%s.wikipedia.org/w/api.php' % lang
+    options = {
+        'action':'query',
+        'list': 'embeddedin',
+        'eilimit': 500,
+        'eititle': eititle,
+        'einamespace': 0,
+        'format': 'json'
+    }
+
+    while not done:
+        if eicontinue != None:
+            options['eicontinue'] = eicontinue
+        url = api_base + '?' + urllib.urlencode(options)
+        logging.info(url)
+        result = simplejson.load(urllib.urlopen(url))
+        articles = result["query"]["embeddedin"]
+        if "query-continue" in result:
+            eicontinue = result["query-continue"]["embeddedin"]["eicontinue"]
+        else:
+            done = True
+
+
+def main():
+    import optparse
+    p = optparse.OptionParser(
+        usage="usage: %prog lang page_title output_file")
+    opts, files = p.parse_args()
+    if len(files) != 3:
+        p.error("Wrong parameters")
+
+    logging.basicConfig(stream=sys.stderr,
+                        level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    get_data(lang=files[0], eititle=files[1], output=files[2])
+
+
+if __name__ == "__main__":
+    main()
