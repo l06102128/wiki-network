9dd5012f0ca83b7251499bdb80a6aa6dcabcf8ae
volpino <fox91@anche.no>
2011-11-07 15:10:06 +0100

    script to merge pywc_revision result

--- pywc_revision_merge.py
+++ pywc_revision_merge.py
@@ -7,18 +7,18 @@ from collections import Counter
 def main():
     output_file = sys.argv[2]
     output_data = {}
-    fielnames = None
-    for filename in os(listdir(sys.argv[1])):
+    fieldnames = None
+    for filename in os.listdir(sys.argv[1]):
         if not filename.endswith(".csv"):
             continue
         current_file = csv.DictReader(open(filename))
         fieldnames = current_file.fieldnames
         for line in current_file:
             if not line["date"] in output_data:
-                output[line["date"]] = Counter()
+                output_data[line["date"]] = Counter()
             for key in line:
                 if key != "date":
-                    output[line["date"]][key] += line[key]
+                    output_data[line["date"]][key] += line[key]
     out = csv.DictWriter(open(output_file, "w"), fieldnames=fieldnames)
     for date in output_data:
         row = output_data[date]
