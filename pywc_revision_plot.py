d74b845aad43cce22246d5dc8ac200ed2d600967
volpino <fox91@anche.no>
2011-12-05 11:22:38 +0100

    namespace filter

--- pywc_revision_plot.py
+++ pywc_revision_plot.py
@@ -148,12 +148,12 @@ def main():
                  help="Sliding window")
     p.add_option('--exclude-less-than', action="store",
                  dest="excludelessthan", type=int,
-                 help="Exclude lines with totals (or dic if -d option is used) " + \
-                      "smaller than this parameter")
+                 help=("Exclude lines with totals (or dic if -d option is "
+                       "used) smaller than this parameter"))
     p.add_option('--exclude-more-than', action="store",
                  dest="excludemorethan", type=int,
-                 help="Exclude lines with totals (or dic if -d option is used) " + \
-                      "greater than this parameter")
+                 help=("Exclude lines with totals (or dic if -d option is "
+                       "used) greater than this parameter"))
     p.add_option('-s', '--start', action="store",
         dest='start', type="yyyymmdd", metavar="YYYYMMDD", default=None,
         help="Look for revisions starting from this date")
@@ -162,6 +162,8 @@ def main():
         help="Look for revisions until this date")
     p.add_option('-d', '--dic', action="store_true", dest="dic", default=False,
                  help="Calculate percentage over dic column instead of total")
+    p.add_option('-n', '--namespaces', action="store", dest="namespaces",
+                 help="Output only selected namespaces (comma separated)")
     opts, files = p.parse_args()
 
     if len(files) != 2:
@@ -197,112 +199,113 @@ def main():
             namespaces[row[ns_index]] = [row]
 
     pdf_pag = PdfPages(files[1])
+    opts.namespaces = opts.namespaces.split(",") if opts.namespaces else None
     for ns in namespaces:
-        logging.info("Processing namespace: %s", ns)
-        # Creates a matrix (list) with percentages of the occurrencies of every
-        # category. Don't count id, total, text, ignore columns. If onlycols is set
-        # consider only them.
-        mat = []
-        timestamps = []
-        totals = []
-        tot_index = -3
-        if opts.dic:
-            tot_index = -5
+        if (not opts.namespaces) or (ns in opts.namespaces):
+            logging.info("Processing namespace: %s", ns)
+            # Creates a matrix (list) with percentages of the occurrencies of every
+            # category. Don't count id, total, text, ignore columns. If onlycols is set
+            # consider only them.
+            mat = []
+            timestamps = []
+            totals = []
+            tot_index = -3
+            if opts.dic:
+                tot_index = -5
 
-        for line in namespaces[ns]:
-            #filter only pages with total (or dic is -d) greater or smaller than X
-            if opts.excludemorethan:
-                if float(line[tot_index]) > opts.excludemorethan:
-                    continue
-            if opts.excludelessthan:
-                if float(line[tot_index]) < opts.excludelessthan:
-                    continue
+            for line in namespaces[ns]:
+                #filter only pages with total (or dic is -d) greater or smaller than X
+                if opts.excludemorethan:
+                    if float(line[tot_index]) > opts.excludemorethan:
+                        continue
+                if opts.excludelessthan:
+                    if float(line[tot_index]) < opts.excludelessthan:
+                        continue
 
-            mat.append([x for x in _gen_data(line, to_skip,
-                                             ignorecols, onlycols)])
-            totals.append(float(line[tot_index]))
-            timestamps.append(dt.strptime(line[opts.id_col], "%Y/%m/%d"))
+                mat.append([x for x in _gen_data(line, to_skip,
+                                                 ignorecols, onlycols)])
+                totals.append(float(line[tot_index]))
+                timestamps.append(dt.strptime(line[opts.id_col], "%Y/%m/%d"))
 
-        mat = np.array(mat, dtype=np.float).transpose()
-        logging.info("Input file read. Ready to plot")
+            mat = np.array(mat, dtype=np.float).transpose()
+            logging.info("Input file read. Ready to plot")
 
-        with Timr("Plotting"):
-            for i, series in enumerate(mat):
-                logging.info("Plotting page %d", i+1)
+            with Timr("Plotting"):
+                for i, series in enumerate(mat):
+                    logging.info("Plotting page %d", i+1)
 
-                # Don't plot zeros and skip zero revisions!
-                #ser = [x for x in series if x != 0]
-                #time = [x for k, x in enumerate(timestamps) if series[k] != 0]
-                #tot = [x for k, x in enumerate(totals) if series[k] != 0]
-                ser = [x for k, x in enumerate(series) \
-                       if (not opts.start or timestamps[k] >= opts.start) and \
-                          (not opts.end or timestamps[k] <= opts.end)]
-                time = [x for k, x in enumerate(timestamps) \
-                        if (not opts.start or x >= opts.start) and \
-                           (not opts.end or x <= opts.end)]
-                tot = [x for k, x in enumerate(totals) \
-                       if (not opts.start or timestamps[k] >= opts.start) and \
-                          (not opts.end or timestamps[k] <= opts.end)]
+                    # Don't plot zeros and skip zero revisions!
+                    #ser = [x for x in series if x != 0]
+                    #time = [x for k, x in enumerate(timestamps) if series[k] != 0]
+                    #tot = [x for k, x in enumerate(totals) if series[k] != 0]
+                    ser = [x for k, x in enumerate(series) \
+                           if (not opts.start or timestamps[k] >= opts.start) and \
+                              (not opts.end or timestamps[k] <= opts.end)]
+                    time = [x for k, x in enumerate(timestamps) \
+                            if (not opts.start or x >= opts.start) and \
+                               (not opts.end or x <= opts.end)]
+                    tot = [x for k, x in enumerate(totals) \
+                           if (not opts.start or timestamps[k] >= opts.start) and \
+                              (not opts.end or timestamps[k] <= opts.end)]
 
-                if opts.smooth and len(time) and len(ser) and len(tot):
-                    time, ser, tot = smooth_values(time, ser, tot,
-                                                   opts.smooth)
+                    if opts.smooth and len(time) and len(ser) and len(tot):
+                        time, ser, tot = smooth_values(time, ser, tot,
+                                                       opts.smooth)
 
-                if opts.window and len(time) and len(ser) and len(tot):
-                    time, ser, tot = collapse_values(time, ser, tot,
-                                                     opts.window)
+                    if opts.window and len(time) and len(ser) and len(tot):
+                        time, ser, tot = collapse_values(time, ser, tot,
+                                                         opts.window)
 
-                mean = float(sum(series)) / len(series)
-                #rel_mean is the mean for the period [opts.end, opts.start]
-                rel_mean = float(sum(ser)) / len(ser)
+                    mean = float(sum(series)) / len(series)
+                    #rel_mean is the mean for the period [opts.end, opts.start]
+                    rel_mean = float(sum(ser)) / len(ser)
 
-                if opts.perc:
-                    try:
-                        mean = float(sum(series)) / sum(totals)
-                        rel_mean = float(sum(ser)) / sum(tot)
-                    except ZeroDivisionError:
-                        mean = 0
-                        rel_mean = 0
-                    # Calculate percentages
-                    ser = [calc_perc(x, tot[k]) for k, x in enumerate(ser)]
-                    # Set axis limit 0-1 IS IT GOOD OR BAD?
-                    #axis.set_ylim(0, 1)
-                    plt.ylabel("%")
+                    if opts.perc:
+                        try:
+                            mean = float(sum(series)) / sum(totals)
+                            rel_mean = float(sum(ser)) / sum(tot)
+                        except ZeroDivisionError:
+                            mean = 0
+                            rel_mean = 0
+                        # Calculate percentages
+                        ser = [calc_perc(x, tot[k]) for k, x in enumerate(ser)]
+                        # Set axis limit 0-1 IS IT GOOD OR BAD?
+                        #axis.set_ylim(0, 1)
+                        plt.ylabel("%")
 
-                first_time = time[0].date()
-                last_time = time[-1].date()
-                plt.clf()
-                plt.subplots_adjust(bottom=0.25)
-                plt.xticks(rotation=90)
-                fig = plt.gcf()
-                fig.set_size_inches(11.7, 8.3)
-                axis = plt.gca()
-                axis.xaxis.set_major_formatter(md.DateFormatter('%Y-%m-%d'))
-                axis.set_xlim(matplotlib.dates.date2num(first_time),
-                              matplotlib.dates.date2num(last_time))
-                if last_time - first_time < timedelta(days=30):
-                    axis.xaxis.set_major_locator(md.DayLocator(interval=1))
-                    axis.xaxis.set_minor_locator(md.DayLocator(interval=1))
-                else:
-                    axis.xaxis.set_minor_locator(md.MonthLocator(interval=1))
-                    rule = md.rrulewrapper(md.MONTHLY, interval=4)
-                    auto_loc = md.RRuleLocator(rule)
-                    axis.xaxis.set_major_locator(auto_loc)
-                axis.tick_params(labelsize='x-small')
-                plt.xlabel("Revisions Timestamp")
+                    first_time = time[0].date()
+                    last_time = time[-1].date()
+                    plt.clf()
+                    plt.subplots_adjust(bottom=0.25)
+                    plt.xticks(rotation=90)
+                    fig = plt.gcf()
+                    fig.set_size_inches(11.7, 8.3)
+                    axis = plt.gca()
+                    axis.xaxis.set_major_formatter(md.DateFormatter('%Y-%m-%d'))
+                    axis.set_xlim(matplotlib.dates.date2num(first_time),
+                                  matplotlib.dates.date2num(last_time))
+                    if last_time - first_time < timedelta(days=30):
+                        axis.xaxis.set_major_locator(md.DayLocator(interval=1))
+                        axis.xaxis.set_minor_locator(md.DayLocator(interval=1))
+                    else:
+                        axis.xaxis.set_minor_locator(md.MonthLocator(interval=1))
+                        rule = md.rrulewrapper(md.MONTHLY, interval=4)
+                        auto_loc = md.RRuleLocator(rule)
+                        axis.xaxis.set_major_locator(auto_loc)
+                    axis.tick_params(labelsize='x-small')
+                    plt.xlabel("Revisions Timestamp")
 
-                if len(time) and len(ser):
-                    if opts.window:
-                        time = [t.date() for t in time]
-                    logging.info("Mean: %f", mean)
-                    logging.info("Relative Mean: %f", rel_mean)
-                    plt.plot(matplotlib.dates.date2num(time), ser, "b.-")
-                    plt.axhline(y=mean, color="r")
-                    plt.title("%s - %s - Mean: %.5f - Relative mean: %.5f" % \
-                              (ns, header[i], round(mean, 5),
-                               round(rel_mean, 5)))
-                    pdf_pag.savefig()
-
+                    if len(time) and len(ser):
+                        if opts.window:
+                            time = [t.date() for t in time]
+                        logging.info("Mean: %f", mean)
+                        logging.info("Relative Mean: %f", rel_mean)
+                        plt.plot(matplotlib.dates.date2num(time), ser, "b.-")
+                        plt.axhline(y=mean, color="r")
+                        plt.title("%s - %s - Mean: %.5f - Relative mean: %.5f" % \
+                                  (ns, header[i], round(mean, 5),
+                                   round(rel_mean, 5)))
+                        pdf_pag.savefig()
     pdf_pag.close()
 
 if __name__ == "__main__":
